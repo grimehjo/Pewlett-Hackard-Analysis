4351ee1306c4670fbc6c4c9d93a4bc3a3d495c1d
Joe Grimeh <josephgrimeh@yahoo.com>
2021-06-27 22:29:12 -0400

    pushing to github fixes to queries and schema made during troubleshooting with the challenge

--- queries.sql
+++ queries.sql
@@ -9,22 +9,26 @@ FROM employees
 WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
 
 -- Retirement eligibility
+
 SELECT first_name, last_name
 FROM employees
 WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
 
 -- Retirement eligibility
+
 SELECT first_name, last_name
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 
 -- Retirement eligibility
+
 SELECT first_name, last_name
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 -- Number of employees retiring
+
 SELECT COUNT(first_name)
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
@@ -46,15 +50,19 @@ SELECT * FROM retirement_info;
 DROP TABLE retirement_info;
 
 -- Create new table for retiring employees
+
 SELECT emp_no, first_name, last_name
 INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
 -- Check the table
+
 SELECT * FROM retirement_info;
 
 -- Joining departments and dept_manager tables
+
 SELECT departments.dept_name,
      dept_manager.emp_no,
      dept_manager.from_date,
@@ -64,15 +72,17 @@ INNER JOIN dept_manager
 ON departments.dept_no = dept_manager.dept_no;
 
 -- Joining retirement_info and dept_emp tables
+
 SELECT retirement_info.emp_no,
     retirement_info.first_name,
-retirement_info.last_name,
+	retirement_info.last_name,
     dept_emp.to_date
 	FROM retirement_info
 	LEFT JOIN dept_emp
 	ON retirement_info.emp_no = dept_emp.emp_no;
 	
 -- Joining retirement_info and dept_emp tables
+
 SELECT ri.emp_no,
     ri.first_name,
 ri.last_name,
@@ -82,6 +92,7 @@ LEFT JOIN dept_emp as de
 ON ri.emp_no = de.emp_no;
 
 -- Joining departments and dept_manager tables
+
 SELECT d.dept_name,
      dm.emp_no,
      dm.from_date,
@@ -101,6 +112,7 @@ ON ri.emp_no = de.emp_no
 WHERE de.to_date = ('9999-01-01');
 
 -- Employee count by department number
+
 SELECT COUNT(ce.emp_no), de.dept_no
 FROM current_emp as ce
 LEFT JOIN dept_emp as de
@@ -108,6 +120,7 @@ ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no;
 
 -- Employee count by department number
+
 SELECT COUNT(ce.emp_no), de.dept_no
 FROM current_emp as ce
 LEFT JOIN dept_emp as de
@@ -121,14 +134,14 @@ SELECT * FROM salaries
 ORDER BY to_date DESC;
 
 SELECT emp_no, first_name, last_name
-INTO retirement_info
+INTO retirement_dept
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT emp_no,
     first_name,
-last_name,
+	last_name,
     gender
 INTO emp_info
 FROM employees
@@ -143,18 +156,16 @@ SELECT e.emp_no,
     de.to_date
 INTO emp_info
 FROM employees as e
-
 INNER JOIN salaries as s
 ON (e.emp_no = s.emp_no)
-
 INNER JOIN dept_emp as de
 ON (e.emp_no = de.emp_no)
-
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
      AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
 	 AND (de.to_date = '9999-01-01');
 	 
 -- List of managers per department
+
 SELECT  dm.dept_no,
         d.dept_name,
         dm.emp_no,
@@ -179,3 +190,84 @@ INNER JOIN dept_emp AS de
 ON (ce.emp_no = de.emp_no)
 INNER JOIN departments AS d
 ON (de.dept_no = d.dept_no);
+
+SELECT * FROM dept_info;
+-- 
+SELECT * FROM retirement_info;
+-- 
+SELECT * FROM departments;
+
+
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name, d.dept_name
+
+
+FROM retirement_info as ri
+INNER JOIN dept_emp AS de
+ON (ri.emp_no=de.emp_no)
+INNER JOIN departments as d
+ON (de.dept_no=d.dept_no)
+WHERE d.dept_name ='Sales';
+
+
+SELECT * FROM dept_info
+
+
+WHERE dept_name = 'Sales';
+
+
+SELECT * FROM dept_info;
+-- 
+SELECT * FROM retirement_info;
+-- 
+SELECT * FROM departments;
+
+
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name, d.dept_name
+INTO retirement_sales_dev
+FROM retirement_info as ri
+INNER JOIN dept_emp AS de
+ON (ri.emp_no=de.emp_no)
+INNER JOIN departments as d
+ON (de.dept_no=d.dept_no)
+WHERE d.dept_name IN ('Sales','Development');
+
+
+SELECT DISTINCT ON (e.emp_no)  d.dept_name, e.emp_no 
+INTO dep_title
+FROM Employees as e
+JOIN dept_emp as de
+ON (e.emp_no=de.emp_no)
+JOIN Departments as d on
+de.dept_no = d.dept_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date ='9999-01-01')
+
+SELECT dt.dept_name , t.title,  COUNT(dt.emp_no) 
+INTO dept_title_retire
+FROM dep_title as dt
+JOIN Titles as t ON
+dt.emp_no = t.emp_no
+GROUP BY dept_name , t.title
+;
+
+
+SELECT DISTINCT ON (e.emp_no) dept_name, e.emp_no 
+INTO dept_title_A
+FROM Employees as e
+JOIN dept_emp as de
+ON (e.emp_no=de.emp_no)
+join Departments as d on
+de.dept_no = d.dept_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (de.to_date ='9999-01-01');
+			   
+SELECT dt.dept_name , title,  COUNT(dt.emp_no)
+INTO dept_title_5255
+FROM dept_title_A as dt
+JOIN Titles t on
+dt.emp_no = t.emp_no
+GROUP BY dept_name , t.title;
--- schema.sql
+++ schema.sql
@@ -1,4 +1,5 @@
 -- Creating tables for PH-EmployeeDB
+
 CREATE TABLE departments (
 	dept_no VARCHAR(4) NOT NULL,
 	dept_name VARCHAR(40) NOT NULL,
@@ -47,10 +48,10 @@ CREATE TABLE dept_emp (
 	
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
-	title VARCHAR(4) NOT NULL,
+	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title, from_date)
 );
 
